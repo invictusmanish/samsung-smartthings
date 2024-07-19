b64e8f1db0531701c0114714a4fdc6c4a21874a9
Manish Jain <manish.j2@samsung.com>
2024-07-20 01:27:48 +0530

    hotspot placement fix

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -13,7 +13,7 @@ import {
 const Landing = () => {
   return (
     <section className="bg-hero">
-      <div className="px-4 mx-auto py-8">
+      <div className="pb-4 pt-8">
         <div className="text-center max-w-screen-sm mx-auto">
           <Text as="caption">With SmartThings</Text>
           <Text as="title">Your home speaks you</Text>
