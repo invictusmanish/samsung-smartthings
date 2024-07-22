d11112eeb8d4b21fe84248a8fb6468c5b077ffe3
AbhayInv <abhay@invictusdata.ai>
2024-07-22 16:37:49 +0530

    carlita configuration

---
+++ src/pages/Landing/Usecases/Carlita.js
@@ -0,0 +1,142 @@
+import { useState } from 'react';
+import VideoPlayer from '../../../components/UI/VideoPlayer/VideoPlayer';
+import UpArrow from '../../../assets/images/icons/up-arrow.svg';
+import backgroundImage from '../../../assets/images/Usecases/carlita/Carlita-BG.jpg';
+import second_bg from '../../../assets/images/Usecases/carlita/carlita_second.png';
+import introVideo from '../../../assets/images/Usecases/carlita/Carlita_Intro.mp4';
+import secondVideo from '../../../assets/images/Usecases/carlita/Carlita_second_video.mp4';
+import Text from '../../../components/UI/Text/Text';
+import Hotspot from '../../../components/Hotspot/Hotspot';
+import Modal from '../../../components/UI/Modal/Modal';
+
+function Carlita() {
+  const [showBackground, setShowBackground] = useState(false);
+  const [showText, setShowText] = useState(false);
+  const [playSecondVideo, setPlaySecondVideo] = useState(false);
+  const [showHotspots, setShowHotspots] = useState(false);
+
+  const handleVideoEnd = () => {
+    setShowBackground(true);
+    setShowText(true);
+    setTimeout(() => {
+      setShowText(false);
+      setPlaySecondVideo(true);
+    }, 5000);
+  };
+
+  const handleSecondVideoEnd = () => {
+    setPlaySecondVideo(false);
+    setShowHotspots(true);
+  };
+
+  const handleHotspotClick = (hotspot) => {
+    debugger;
+    console.log(`${hotspot} hotspot clicked`);
+  };
+
+  return (
+    <div className="h-screen w-screen relative">
+      {!showBackground ? (
+        <VideoPlayer
+          videoSrc={introVideo}
+          onVideoEnd={handleVideoEnd}
+        />
+      ) : playSecondVideo ? (
+        <VideoPlayer
+          videoSrc={secondVideo}
+          onVideoEnd={handleSecondVideoEnd}
+        />
+      ) : (
+        <div
+          className="bg-cover bg-center h-full w-full"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        >
+          {showText && (
+            <div className="absolute top-1/4 left-0 px-10 animate-slideInOut space-y-4">
+              <Text
+                as="caption"
+                className="text-white"
+              >
+                Self Care
+              </Text>
+              <Text
+                as="title"
+                className="text-white"
+              >
+                Enhance your
+                <br />
+                wellness
+              </Text>
+              <Text
+                as="usecase-fading-text"
+                className="text-white"
+              >
+                Discover how SmartThings seamlessly connects
+                <br />
+                your watch, screens, music, and more to create
+                <br />
+                the perfect workout environment.
+              </Text>
+            </div>
+          )}
+
+          {showHotspots && (
+            <>
+              <div
+                className="bg-cover bg-center h-full w-full"
+                style={{ backgroundImage: `url(${second_bg})` }}
+              >
+                <Modal show={true}>
+                  <img
+                    className="mx-auto"
+                    src={UpArrow}
+                    alt={'Arrow'}
+                  />
+                  <Text>
+                    Select a room on this page to see how SmartThings routines
+                    help bring your home to life
+                  </Text>
+                </Modal>
+                <div
+                  className="absolute"
+                  style={{ top: '63%', left: '41%' }}
+                >
+                  <Hotspot
+                    text={'Watch'}
+                    onClick={() => handleHotspotClick('Watch')}
+                  />
+                </div>
+                <div
+                  className="absolute"
+                  style={{ top: '51%', left: '56%' }}
+                >
+                  <Hotspot onClick={() => handleHotspotClick('Z Flip')} />
+                </div>
+                <div
+                  className="absolute"
+                  style={{ top: '72%', left: '75%' }}
+                >
+                  <Hotspot onClick={() => handleHotspotClick('Sero')} />
+                </div>
+                <div
+                  className="absolute"
+                  style={{ top: '62.5%', left: '30.5%' }}
+                >
+                  <Hotspot onClick={() => handleHotspotClick('Smart Plug')} />
+                </div>
+                <div
+                  className="absolute"
+                  style={{ top: '24%', left: '21.5%' }}
+                >
+                  <Hotspot onClick={() => handleHotspotClick('Frame')} />
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Carlita;
--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Landing from '../pages/Landing/Landing';
 import { routes } from '../utility/constant';
+import Carlita from '../pages/Landing/Usecases/Carlita';
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,10 @@ const AppRouter = () => {
       <Route
         path={routes.HOME}
         element={<Landing />}
+      />
+      <Route
+        path={routes.USE_CASE_CARLITA}
+        element={<Carlita />}
       />
     </Routes>
   );
