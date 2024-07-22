176e910eccda21648cb3f3f6ad71e948ee32559c
AbhayInv <abhay@invictusdata.ai>
2024-07-22 13:25:59 +0530

    final header

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -13,45 +13,46 @@ import {
 const Landing = () => {
   return (
     <section className="bg-hero">
-      <div className="pb-4 pt-8">
+      <div className="px-4 mx-auto py-8">
         <div className="text-center max-w-screen-sm mx-auto">
           <Text as="caption">With SmartThings</Text>
           <Text as="title">Your home speaks you</Text>
           <Text>
             Select a room below to explore how SmartThings app seamlessly
+            <br />
             integrates your home&apos;s smart devices.
           </Text>
         </div>
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 gap-2">
           <LandingRoom
             {...AISHA_DATA}
-            className="bg-[rgba(82,193,186,0.80)]"
+            buttonColor="bg-[rgba(82,193,186,0.80)]"
             navigateTo={routes.USE_CASE_AISHA}
           />
 
           <LandingRoom
             {...EDDIE_DATA}
-            className="bg-[rgba(134,25,90,0.80)]"
+            buttonColor="bg-[rgba(134,25,90,0.80)]"
             navigateTo={routes.USE_CASE_EDDIE}
           />
           <LandingRoom
             {...ERIC_DATA}
-            className="bg-[rgba(135,156,129,0.80)]"
+            buttonColor="bg-[rgba(135,156,129,0.80)]"
             navigateTo={routes.USE_CASE_ERIC}
           />
           <LandingRoom
             {...CARLITA_DATA}
-            className="bg-[rgba(222,88,9,0.80)]"
+            buttonColor="bg-[rgba(222,88,9,0.80)]"
             navigateTo={routes.USE_CASE_CARLITA}
           />
           <LandingRoom
             {...MALIK_DATA}
-            className="bg-[rgba(167,224,181,0.80)]"
+            buttonColor="bg-[rgba(167,224,181,0.80)]"
             navigateTo={routes.USE_CASE_MALIK}
           />
           <LandingRoom
             {...FRAN_DATA}
-            className="bg-[rgba(93,171,225,0.80)]"
+            buttonColor="bg-[rgba(93,171,225,0.80)]"
             navigateTo={routes.USE_CASE_FRAN}
           />
         </div>
