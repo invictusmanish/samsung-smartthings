cfe879648a302c58176d36573d51740ad2459230
AbhayInv <abhay@invictusdata.ai>
2024-07-23 14:34:13 +0530

    landing page popup

--- src/components/LandingRoom/LandingRoom.js
+++ src/components/LandingRoom/LandingRoom.js
@@ -4,36 +4,38 @@ import Button from '../UI/Button/Button';
 
 const LandingRoom = ({ image, title, buttonColor, hoverImage, navigateTo }) => {
   return (
-    <div className="relative group">
-      <div className=" relative image-container grid grid-cols-1 grid-rows-1">
-        <img
-          src={baseImagePath(image)}
-          alt={title}
-          className="opacity-100 w-full h-full object-cover col-start-1 row-start-1 group-hover:opacity-30"
-        />
-        <img
-          src={baseImagePath(hoverImage)}
-          alt={title}
-          className="opacity-0 w-full h-full object-cover col-start-1 row-start-1 group-hover:opacity-80"
-        />
+    <>
+      <div className="relative group">
+        <div className=" relative image-container grid grid-cols-1 grid-rows-1">
+          <img
+            src={baseImagePath(image)}
+            alt={title}
+            className="opacity-100 w-full h-full object-cover col-start-1 row-start-1 group-hover:opacity-30"
+          />
+          <img
+            src={baseImagePath(hoverImage)}
+            alt={title}
+            className="opacity-0 w-full h-full object-cover col-start-1 row-start-1 group-hover:opacity-80"
+          />
+        </div>
+        <div className="absolute inset-0 flex flex-col items-center justify-center opacity-100 -top-8 -left-1/4 group-hover:opacity-0 transition-opacity duration-300">
+          <HotspotGif />
+          <Button
+            btnTheme="custom"
+            title={title}
+            className={`text-white ${buttonColor} py-1 font-bold text-base px-4 opacity-80`}
+          />
+        </div>
+        <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 -left-1/4 group-hover:opacity-100 transition-opacity duration-300">
+          <Button
+            btnTheme="custom"
+            title={'Explore Room'}
+            className={`border-2 border-white text-white py-1 px-4 bg-transparent`}
+            link={navigateTo}
+          />
+        </div>
       </div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center opacity-100 -top-8 -left-1/4 group-hover:opacity-0 transition-opacity duration-300">
-        <HotspotGif />
-        <Button
-          btnTheme="custom"
-          title={title}
-          className={`text-white ${buttonColor} py-1 font-bold text-base px-4 opacity-80`}
-        />
-      </div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 -left-1/4 group-hover:opacity-100 transition-opacity duration-300">
-        <Button
-          btnTheme="custom"
-          title={'Explore Room'}
-          className={`border-2 border-white text-white py-1 px-4 bg-transparent`}
-          link={navigateTo}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -12,6 +12,8 @@ import {
   MALIK_DATA,
 } from './Landing.data';
 import introVideo from '../../assets/videos/LandingPage_Intro_Desktop.mp4';
+import Modal from '../../components/UI/Modal/Modal';
+import UpArrow from '../../assets/images/icons/up-arrow.svg';
 
 const Landing = () => {
   const [showBackground, setShowBackground] = useState(false);
@@ -28,51 +30,65 @@ const Landing = () => {
           onVideoEnd={handleVideoEnd}
         />
       ) : (
-        <div className="mx-auto py-8">
-          <div className="text-center max-w-screen-sm mx-auto">
-            <Text as="caption">With SmartThings</Text>
-            <Text as="title">Your home speaks you</Text>
-            <Text>
-              Select a room below to explore how SmartThings app seamlessly
-              <br />
-              integrates your home&apos;s smart devices.
+        <>
+          <Modal show={true}>
+            <img
+              className=" mx-auto mb-4 top-40"
+              src={UpArrow}
+              alt={'Arrow'}
+            />
+            <Text className="font-bold text-center">
+              Select a room on this page to see how
+              <br /> SmartThings routines help bring your
+              <br /> home to life
             </Text>
-          </div>
+          </Modal>
+          <div className="mx-auto py-8">
+            <div className="text-center max-w-screen-sm mx-auto">
+              <Text as="caption">With SmartThings</Text>
+              <Text as="title">Your home speaks you</Text>
+              <Text>
+                Select a room below to explore how SmartThings app seamlessly
+                <br />
+                integrates your home&apos;s smart devices.
+              </Text>
+            </div>
 
-          <div className="grid grid-cols-3 gap-2">
-            <LandingRoom
-              {...AISHA_DATA}
-              buttonColor="bg-[rgba(82,193,186,0.80)]"
-              navigateTo={routes.USE_CASE_AISHA}
-            />
+            <div className="grid grid-cols-3 gap-2">
+              <LandingRoom
+                {...AISHA_DATA}
+                buttonColor="bg-[rgba(82,193,186,0.80)]"
+                navigateTo={routes.USE_CASE_AISHA}
+              />
 
-            <LandingRoom
-              {...EDDIE_DATA}
-              buttonColor="bg-[rgba(134,25,90,0.80)]"
-              navigateTo={routes.USE_CASE_EDDIE}
-            />
-            <LandingRoom
-              {...ERIC_DATA}
-              buttonColor="bg-[rgba(135,156,129,0.80)]"
-              navigateTo={routes.USE_CASE_ERIC}
-            />
-            <LandingRoom
-              {...CARLITA_DATA}
-              buttonColor="bg-[rgba(222,88,9,0.80)]"
-              navigateTo={routes.USE_CASE_CARLITA}
-            />
-            <LandingRoom
-              {...MALIK_DATA}
-              buttonColor="bg-[rgba(167,224,181,0.80)]"
-              navigateTo={routes.USE_CASE_MALIK}
-            />
-            <LandingRoom
-              {...FRAN_DATA}
-              buttonColor="bg-[rgba(93,171,225,0.80)]"
-              navigateTo={routes.USE_CASE_FRAN}
-            />
+              <LandingRoom
+                {...EDDIE_DATA}
+                buttonColor="bg-[rgba(134,25,90,0.80)]"
+                navigateTo={routes.USE_CASE_EDDIE}
+              />
+              <LandingRoom
+                {...ERIC_DATA}
+                buttonColor="bg-[rgba(135,156,129,0.80)]"
+                navigateTo={routes.USE_CASE_ERIC}
+              />
+              <LandingRoom
+                {...CARLITA_DATA}
+                buttonColor="bg-[rgba(222,88,9,0.80)]"
+                navigateTo={routes.USE_CASE_CARLITA}
+              />
+              <LandingRoom
+                {...MALIK_DATA}
+                buttonColor="bg-[rgba(167,224,181,0.80)]"
+                navigateTo={routes.USE_CASE_MALIK}
+              />
+              <LandingRoom
+                {...FRAN_DATA}
+                buttonColor="bg-[rgba(93,171,225,0.80)]"
+                navigateTo={routes.USE_CASE_FRAN}
+              />
+            </div>
           </div>
-        </div>
+        </>
       )}
     </section>
   );
