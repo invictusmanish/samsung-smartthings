9671d93d4b7d6e4df7bcdf5c3de140781d54eceb
Manish Jain <manish.j2@samsung.com>
2024-07-24 11:43:15 +0530

    bug fix

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -17,7 +17,7 @@ import {
   FRAN_DATA_MOB,
   MALIK_DATA,
   MALIK_DATA_MOB,
-  LANDING_DATA
+  LANDING_DATA,
 } from './Landing.data';
 import desktopIntroVideo from '../../assets/videos/LandingPage_Intro_Desktop.mp4';
 import mobileIntroVideo from '../../assets/videos/LandingPage_Mobile.mp4';
@@ -29,7 +29,7 @@ const Landing = () => {
   const [showBackground, setShowBackground] = useState(false);
 
   const handleVideoEnd = () => {
-    console.log("bg" ,showBackground);
+    console.log('bg', showBackground);
     setShowBackground(true);
   };
   const items = [
@@ -72,111 +72,109 @@ const Landing = () => {
   ];
   if (!showBackground) {
     return (
-      <AppLayout isShowHeader = {false}>
-      <section>
-        <div className='bg-hero hidden lg:block video-container'>
-        <VideoPlayer
-          videoSrc={desktopIntroVideo}
-          onVideoEnd={handleVideoEnd}
-        />
-        </div>
-        <div className='lg:hidden bg-mobile-hero h-screen video-container'>
-        <VideoPlayer
-          videoSrc={mobileIntroVideo}
-          onVideoEnd={handleVideoEnd}
-        />
-        </div>
-      </section>
+      <AppLayout isShowHeader={true}>
+        <section>
+          <div className="bg-hero hidden lg:block video-container">
+            <VideoPlayer
+              videoSrc={desktopIntroVideo}
+              onVideoEnd={handleVideoEnd}
+            />
+          </div>
+          <div className="lg:hidden bg-mobile-hero h-screen video-container">
+            <VideoPlayer
+              videoSrc={mobileIntroVideo}
+              onVideoEnd={handleVideoEnd}
+            />
+          </div>
+        </section>
       </AppLayout>
     );
   }
   return (
     <AppLayout>
-    <section>
-      <div className="lg:hidden bg-mobile-hero h-screen">
-        <div className="text-center max-w-screen-sm mx-auto py-3">
-          <Text
-            as="custom"
-            className="font-semibold"
-          >
-            {LANDING_DATA.caption}
-          </Text>
-          <Text
-            as="custom"
-            className="font-bold text-2xl py-2"
-          >
-            {LANDING_DATA.title}
-          </Text>
-          <Text>
-            {LANDING_DATA.description}
-          </Text>
-        </div>
-        <Slider items={items} />
-      </div>
-      <div className="bg-hero h-screen hidden lg:block">
-        <div className="pb-4 pt-8">
-          <div className="text-center max-w-screen-sm mx-auto">
-            <Text as="caption">{LANDING_DATA.caption}</Text>
-            <Text as="title">{LANDING_DATA.title}</Text>
-            <Text>
-              Select a room below to explore how SmartThings app seamlessly
-              <br />
-              integrates your home&apos;s smart devices.
+      <section>
+        <div className="lg:hidden bg-mobile-hero h-screen">
+          <div className="text-center max-w-screen-sm mx-auto py-3">
+            <Text
+              as="custom"
+              className="font-semibold"
+            >
+              {LANDING_DATA.caption}
             </Text>
+            <Text
+              as="custom"
+              className="font-bold text-2xl py-2"
+            >
+              {LANDING_DATA.title}
+            </Text>
+            <Text>{LANDING_DATA.description}</Text>
           </div>
-          <div className="grid grid-cols-3">
-            <LandingRoom
-              key="aisha"
-              {...AISHA_DATA}
-              className="bg-[rgba(82,193,186,0.80)]"
-              navigateTo={routes.USE_CASE_AISHA}
-            />
-            <LandingRoom
-              key="eddie"
-              {...EDDIE_DATA}
-              className="bg-[rgba(134,25,90,0.80)]"
-              navigateTo={routes.USE_CASE_EDDIE}
-            />
-            <LandingRoom
-              key="eric"
-              {...ERIC_DATA}
-              className="bg-[rgba(135,156,129,0.80)]"
-              navigateTo={routes.USE_CASE_ERIC}
-            />
-            <LandingRoom
-              key="carlita"
-              {...CARLITA_DATA}
-              className="bg-[rgba(222,88,9,0.80)]"
-              navigateTo={routes.USE_CASE_CARLITA}
-            />
-            <LandingRoom
-              key="malik"
-              {...MALIK_DATA}
-              className="bg-[rgba(167,224,181,0.80)]"
-              navigateTo={routes.USE_CASE_MALIK}
-            />
-            <LandingRoom
-              key="fran"
-              {...FRAN_DATA}
-              className="bg-[rgba(93,171,225,0.80)]"
-              navigateTo={routes.USE_CASE_FRAN}
-            />
+          <Slider items={items} />
+        </div>
+        <div className="bg-hero h-screen hidden lg:block">
+          <div className="pb-4 pt-8">
+            <div className="text-center max-w-screen-sm mx-auto">
+              <Text as="caption">{LANDING_DATA.caption}</Text>
+              <Text as="title">{LANDING_DATA.title}</Text>
+              <Text>
+                Select a room below to explore how SmartThings app seamlessly
+                <br />
+                integrates your home&apos;s smart devices.
+              </Text>
+            </div>
+            <div className="grid grid-cols-3">
+              <LandingRoom
+                key="aisha"
+                {...AISHA_DATA}
+                className="bg-[rgba(82,193,186,0.80)]"
+                navigateTo={routes.USE_CASE_AISHA}
+              />
+              <LandingRoom
+                key="eddie"
+                {...EDDIE_DATA}
+                className="bg-[rgba(134,25,90,0.80)]"
+                navigateTo={routes.USE_CASE_EDDIE}
+              />
+              <LandingRoom
+                key="eric"
+                {...ERIC_DATA}
+                className="bg-[rgba(135,156,129,0.80)]"
+                navigateTo={routes.USE_CASE_ERIC}
+              />
+              <LandingRoom
+                key="carlita"
+                {...CARLITA_DATA}
+                className="bg-[rgba(222,88,9,0.80)]"
+                navigateTo={routes.USE_CASE_CARLITA}
+              />
+              <LandingRoom
+                key="malik"
+                {...MALIK_DATA}
+                className="bg-[rgba(167,224,181,0.80)]"
+                navigateTo={routes.USE_CASE_MALIK}
+              />
+              <LandingRoom
+                key="fran"
+                {...FRAN_DATA}
+                className="bg-[rgba(93,171,225,0.80)]"
+                navigateTo={routes.USE_CASE_FRAN}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <Modal show={true}>
-        <img
-          className=" mx-auto mb-4 top-40"
-          src={UpArrow}
-          alt={'Arrow'}
-        />
-        <Text className="font-bold text-center">
-          Select a room on this page to see how
-          <br /> SmartThings routines help bring your
-          <br /> home to life
-        </Text>
-      </Modal>
-    </section>
+        <Modal show={true}>
+          <img
+            className=" mx-auto mb-4 top-40"
+            src={UpArrow}
+            alt={'Arrow'}
+          />
+          <Text className="font-bold text-center">
+            Select a room on this page to see how
+            <br /> SmartThings routines help bring your
+            <br /> home to life
+          </Text>
+        </Modal>
+      </section>
     </AppLayout>
   );
 };
